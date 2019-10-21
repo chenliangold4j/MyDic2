0584f36513d291f498c9529e76a4495347469e36
phantom <chenliangxingji1@163.com>
2019-10-21 18:30:13 +0800

    delete

--- src/main/java/self/liang/jedis/example/NoPassEnter.java
+++ src/main/java/self/liang/jedis/example/NoPassEnter.java
@@ -12,22 +12,22 @@ public class NoPassEnter {
         Jedis jedis = new Jedis("192.168.0.155");
         System.out.println("连接成功");
         //查看服务是否运行
-        System.out.println("服务正在运行: "+jedis.ping());
+        System.out.println("服务正在运行: " + jedis.ping());
         jedis.set("start", "test");
-        System.out.println("redis 存储的字符串为: "+ jedis.get("start"));
+        System.out.println("redis 存储的字符串为: " + jedis.get("start"));
 
         jedis.lpush("site-list", "Runoob");
         jedis.lpush("site-list", "Google");
         jedis.lpush("site-list", "Taobao");
         // 获取存储的数据并输出
-        List<String> list = jedis.lrange("site-list", 0 ,2);
-        for(int i=0; i<list.size(); i++) {
-            System.out.println("列表项为: "+list.get(i));
+        List<String> list = jedis.lrange("site-list", 0, 2);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("列表项为: " + list.get(i));
         }
 
         Set<String> keys = jedis.keys("*");
-        Iterator<String> it=keys.iterator() ;
-        while(it.hasNext()){
+        Iterator<String> it = keys.iterator();
+        while (it.hasNext()) {
             String key = it.next();
             System.out.println(key);
         }
--- src/main/java/self/liang/jvm/example/FirstTest.java
+++
@@ -1,8 +0,0 @@
-package self.liang.jvm.example;
-
-public class FirstTest {
-
-
-
-
-}
